d4ea48ccfe942afc719fbd4700eaab0eb990cac1
aakanksha19 <aakankshanaik19@gmail.com>
2016-11-04 14:20:10 -0700

    Updated make_vocab.py

--- make_vocab.py
+++ make_vocab.py
@@ -6,30 +6,33 @@ from nltk.util import ngrams
 
 input_folder = sys.argv[1]
 vocab = {}
-
+word_id = 0
 for file in os.listdir(input_folder):
 	input = open(os.path.join(input_folder,file),'r')
 	print file
 	for line in input:
-		if "turn" in line:
+		if line.startswith("turn "):
 			continue
 		tokens = word_tokenize(line.split('\t')[0])
 		bigrams = ngrams(tokens,2)
 		trigrams = ngrams(tokens,3)
 		for word in tokens:
 			if word in vocab:
-				vocab[word] += 1
+				continue
 			else:
-				vocab[word] = 1
+				vocab[word] = word_id
+				word_id += 1
 		for word in bigrams:
 			if word in vocab:
-				vocab[word] += 1
+				continue
 			else:
-				vocab[word] = 1
+				vocab[word] = word_id
+				word_id += 1
 		for word in trigrams:
 			if word in vocab:
-				vocab[word] += 1
+				continue
 			else:
-				vocab[word] = 1
+				vocab[word] = word_id
+				word_id += 1
 
 pickle.dump(vocab,open('vocab.pkl','wb'))
