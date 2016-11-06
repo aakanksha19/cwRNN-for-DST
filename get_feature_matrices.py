a0f2fc8661ae143923264c4e231e86209d0bf3ae
aakanksha19 <aakankshanaik19@gmail.com>
2016-11-06 10:59:59 -0800

    adding code to compute features matrices

---
+++ get_feature_matrices.py
@@ -0,0 +1,36 @@
+import pickle
+
+output_pickle = pickle.load(open("../output_features/output_features.p","rb"))
+
+X_train = []
+y_train_method = []
+y_train_request = []
+
+method_slots = ['none','byconstraints','byname','finished','byalternatives']
+request_slots = ['addr','area','food','phone','pricerange','postcode','signature','name','none']
+train_flist = open("../scripts/config/dstc2_train.flist")
+dev_flist = open("../scripts/config/dstc2_dev.flist")
+train_output = {}
+test_output = {}
+
+index = 0
+for i in range(1,33):
+	input_features = pickle.load(open("features/feat"+str(i)+".pkl"))
+	for key in input_features:
+		turns = len(input_features[key].keys())
+		X_train.append([[]]*turns)
+		y_train_method.append([[0]*len(method_slots)]*turns)
+		y_train_request.append([[0]*len(request_slots)]*turns)
+		for turn in range(turns):
+			X_train[index][turn] = input_features[key][turn]
+			for slot in output_pickle[key][turn]['method-label']:
+				y_train_request[index][turn][method_slots.index(slot)] = 1
+			for slot in output_pickle[key][turn]['requested-slots']:
+				y_train_request[index][turn][request_slots.index(slot)] = 1
+	index += 1
+
+pickle.dump(X_train,open("X_train.pkl","wb"))
+pickle.dump(y_train_method,open("y_train_method.pkl","wb"))
+pickle.dump(y_train_request,open("y_train_request.pkl","wb"))
+
+
